dfe64ae19b5b5025d86c58a0c9f231f7189585b3
Gyuzam <gyuzelakhm@gmail.com>
2018-03-23 00:55:04 +0300

    added hometask

---
+++ src/main/java/ru/sbt/jschool/session1/HomeTask.java
@@ -0,0 +1,104 @@
+package ru.sbt.jschool.session1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class HomeTask implements PropertyHelper{
+
+    private String[] args;
+    private String path;
+
+    @Override
+    public String stringValue(String name) {
+
+        // В аргументе
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith(name + "=")){
+                String val = args[i].replaceFirst (name + "=", "");
+                return val;
+            }
+        }
+
+        // Системная настройка
+        String property = System.getProperties().getProperty(name);
+        if (property != null){
+            return property;
+        }
+
+        // Переменная окружения
+        String envvar = System.getenv(name);
+        if (envvar != null){
+            return envvar;
+        }
+
+        // Файл
+        if (path != null) {
+            Properties props = new Properties();
+            try{
+                FileInputStream inputStream = new FileInputStream(path);
+                props.load(inputStream);
+            }
+            catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            property = props.getProperty(name);
+            if (property != null){
+                return property;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Integer integerValue(String name) {
+        String val = stringValue(name);
+        try {
+            Integer number = Integer.parseInt(val);
+            return number;
+        }
+        catch (NumberFormatException ex) {
+            //System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public Double doubleValue(String name) {
+        String val = stringValue(name);
+        try {
+            Double number = Double.parseDouble(val);
+            return number;
+        }
+        catch (NumberFormatException ex) {
+            //System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    HomeTask(String[] args, String path) {
+        this.args = args;
+        this.path = path;
+    }
+
+    public static void main(String[] args) {
+
+        HomeTask ht = new HomeTask(args, "C:/input.properties");
+
+        System.out.println(ht.stringValue("num"));
+
+        Integer intn = ht.integerValue("num");
+
+        if (intn != null) {
+            System.out.println(intn);
+        }
+
+        Double dobn = ht.doubleValue("num");
+        if (dobn != null) {
+            System.out.println(dobn);
+        }
+
+        return;
+    }
+}
